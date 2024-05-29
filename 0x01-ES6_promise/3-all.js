4413506c548db9b9862ec0f88ae0105d60d63860
pasebukky <olubukunolapase@gmail.com>
2024-05-29 11:58:34 +0100

    Include return statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photoData, userData] = results;
       console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
